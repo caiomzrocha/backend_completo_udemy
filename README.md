27eb9f94a7e6962390b7357733bb7193e85c0053
caiomzrocha <caiomzrocha@gmail.com>
2021-12-31 01:59:21 -0300

    INIT

---
+++ README.md
@@ -0,0 +1,1 @@
+# Backend Completo Udemy
---
+++ src/test/java/com/birulino/backend_completo/BackendCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birulino.backend_completo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
